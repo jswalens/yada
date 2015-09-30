3778c0c28e7486c5737878d8f8bc671714817c7f
Janwillem Swalens <jswalens@vub.ac.be>
2015-09-30 14:11:29 +0200

    Finish test.

--- test/yada/mesh_test.clj
+++ test/yada/mesh_test.clj
@@ -11,9 +11,9 @@
       (@#'yada.mesh/parse-line "1 2.3" @#'yada.mesh/str->int @#'yada.mesh/str->double))))
 
 (deftest read-test
+  ; Expect results from C version
   (let [{:keys [n-element mesh]} (read "inputs/633.2")
-        root-element   @(:root-element @mesh)
-        first-in-queue (first (:init-bad-queue @mesh))]
+        root-element   @(:root-element @mesh)]
     (is (= 1264 n-element))
     (is (= 438  (count (:init-bad-queue @mesh))))
     (is (= (list {:x 570110.0 :y 431911.0} {:x 585233.0 :y 413468.0})
@@ -27,6 +27,5 @@
     (is (= false                     (:skinny? root-element)))
     (is (= false                     (:garbage? root-element)))
     (is (= false                     (:referenced? root-element)))
-    ;(is (= (list {:x 570110.0 :y 431911.0} {:x 585233.0 :y 413468.0} {:x 584400 :y 414700})
-    ;       (:coordinates first-in-queue))) -- FIXME: last coordinate inexact
-    ))
+    (is (= (list {:x 570110.0 :y 431911.0} {:x 585233.0 :y 413468.0} {:x 584373.0 :y 414654.0})
+           (:coordinates @(first (:init-bad-queue @mesh)))))))
