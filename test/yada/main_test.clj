5ac02b7d117590b74f382383d69421398bdbc960
Janwillem Swalens <jswalens@vub.ac.be>
2015-10-09 13:03:20 +0200

    Add more tests for main function.

--- test/yada/main_test.clj
+++ test/yada/main_test.clj
@@ -3,23 +3,40 @@
             [yada.main]
             [yada.mesh :as mesh]))
 
-(deftest test-633
+(defn test-all [input-name expect-init-num-element expect-init-num-bad-element
+  expect-final-num-element expect-num-process]
   (let [{mesh :mesh init-num-element :n-element}
-          (mesh/read "inputs/633.2")
+          (mesh/read input-name)
         ; mesh tested in mesh_test
         work-queue
           (@#'yada.main/initialize-work mesh)
         init-num-bad-element
           (count (:elements @work-queue))
-        _ (is (= 1264 init-num-element))    ; from C version
-        _ (is (= 438 init-num-bad-element)) ; from C version
+        _ (is (= expect-init-num-element init-num-element))
+        _ (is (= expect-init-num-bad-element init-num-bad-element))
         {total-num-added :n-added num-process :n-process}
           (@#'yada.main/process mesh work-queue)
         final-num-element
           (+ init-num-element total-num-added)
-        _ (is (= 2678 final-num-element))   ; from C version
-        _ (is (= 705 num-process))          ; from C version
+        _ (is (= expect-final-num-element final-num-element))
+        _ (is (= expect-num-process num-process))
         success?
-          ;(mesh/check mesh final-num-element) - disabled
+          ;(mesh/check mesh final-num-element) - disabled TODO
           true
         _ (is success?)]))
+
+(deftest test-633
+  ; Expected results from C version
+  (test-all "inputs/633.2" 1264 438 2678 705))
+
+(deftest test-dots
+  ; Expected results from C version
+  (test-all "inputs/dots.2" 198 70 586 193))
+
+(deftest test-ladder
+  ; Expected results from C version
+  (test-all "inputs/ladder.2" 254 126 508 123))
+
+(deftest test-spiral
+  ; Expected results from C version
+  (test-all "inputs/spiral.2" 28 8 40 6))
