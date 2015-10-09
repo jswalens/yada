d36687669e62a6dc629898d1b36d6b9b233b1023
Janwillem Swalens <jswalens@vub.ac.be>
2015-10-09 12:58:05 +0200

    Add test for whole program. Fails because of bugs.

--- test/yada/main_test.clj
+++ test/yada/main_test.clj
@@ -1,3 +1,25 @@
 (ns yada.main-test
   (:require [clojure.test :refer :all]
-            [yada.main :refer :all]))
+            [yada.main]
+            [yada.mesh :as mesh]))
+
+(deftest test-633
+  (let [{mesh :mesh init-num-element :n-element}
+          (mesh/read "inputs/633.2")
+        ; mesh tested in mesh_test
+        work-queue
+          (@#'yada.main/initialize-work mesh)
+        init-num-bad-element
+          (count (:elements @work-queue))
+        _ (is (= 1264 init-num-element))    ; from C version
+        _ (is (= 438 init-num-bad-element)) ; from C version
+        {total-num-added :n-added num-process :n-process}
+          (@#'yada.main/process mesh work-queue)
+        final-num-element
+          (+ init-num-element total-num-added)
+        _ (is (= 2678 final-num-element))   ; from C version
+        _ (is (= 705 num-process))          ; from C version
+        success?
+          ;(mesh/check mesh final-num-element) - disabled
+          true
+        _ (is success?)]))
