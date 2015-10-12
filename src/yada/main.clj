df405acb6fdecef752be26c0ea686001704e62ca
Janwillem Swalens <jswalens@vub.ac.be>
2015-10-12 16:54:46 +0200

    Nicer logging of some things + disable logging.

--- src/yada/main.clj
+++ src/yada/main.clj
@@ -1,17 +1,24 @@
 (ns yada.main
   (:gen-class)
-  (:require [random]
+  (:require [clojure.string]
+            [random]
             [priority-queue]
             [yada.element :as element]
             [yada.region :as region]
             [yada.mesh :as mesh]))
 
-(def log println)
+;(def log println)
+(defn log [& _] nil)
 
 (defn- parse-args [args]
   {:angle-constraint 20.0
    :input-prefix     "inputs/spiral.2"
    :num-thread       1})
+
+(defn- elements->str [elements]
+  (->> elements
+    (map element/element->str)
+    (clojure.string/join "\n")))
 
 (defn- initialize-work [mesh]
   ;(mesh/shuffle-bad mesh) - Don't do this, to get deterministic results
@@ -39,9 +46,9 @@
                       (region/refine region element mesh))]
               (dosync
                 (element/set-is-referenced? element false))
-              (println "length bad vector:" (count (:bad-vector @region)))
               (dosync
                 (region/transfer-bad region work-queue))
+              (log "additional bad elements: " (elements->str (:bad-vector @region)))
               (recur (+ n-added added) (inc n-process)))))
         {:n-added n-added :n-process n-process}))))
 
@@ -57,7 +64,7 @@
         work-queue ; This is a heap in the C version
           (initialize-work mesh)
         _ (log "Work queue:")
-        _ (doseq [e (:elements @work-queue)] (log (element/element->str e)))
+        _ (log (elements->str (:elements @work-queue)))
         init-num-bad-element
           (count (:elements @work-queue))
         _ (println "Initial number of mesh elements =" init-num-element)
