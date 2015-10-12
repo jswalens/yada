b224d834133f921d228783620379e9fa56341a76
Janwillem Swalens <jswalens@vub.ac.be>
2015-10-12 16:48:17 +0200

    Change implementation of priority queue so it's closer to the C version (in final order, not in implementation). It's still not the same though.

--- src/priority_queue.clj
+++ src/priority_queue.clj
@@ -3,13 +3,26 @@
 (defn create [cmp]
   "Create an empty priority queue. Its elements will be ordered by comparing the
   result of `(key element)`."
-  (ref {:cmp cmp :elements (list)}))
+  (ref {:cmp cmp :elements []}))
 
 (defn push [queue val]
   "Push `val` in `queue`."
   (dosync
-    (alter queue assoc :elements
-      (sort (:cmp @queue) (cons val (:elements @queue))))))
+    (let [new-elements
+            (loop [old-elements (:elements @queue)
+                   new-elements []]
+              (let [[fst & rst] old-elements]
+                (if (nil? fst)
+                  (conj new-elements val)
+                  (case ((:cmp @queue) fst val)
+                    -1 ; old < val: continue
+                      (recur rst (conj new-elements fst))
+                     0 ; old == val: still continue, val will be inserted AFTER
+                       ; the ones it is equal to
+                      (recur rst (conj new-elements fst))
+                    +1 ; old > val: insert it and end the loop
+                      (into (conj new-elements val) old-elements)))))]
+      (alter queue assoc :elements new-elements))))
 
 (defn pop [queue]
   "Pops element from `queue` and returns it.
@@ -18,7 +31,3 @@
     (when-let [e (first (:elements @queue))]
       (alter queue update-in [:elements] rest)
       e)))
-
-;(defn remove [queue val]
-;  "Remove `val` from `queue`."
-;  (clojure.core/remove #(= % val) queue))
