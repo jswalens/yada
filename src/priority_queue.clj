0c32d0a74456dd70ff116221bce0a566ca8c4e71
Janwillem Swalens <jswalens@vub.ac.be>
2015-10-15 11:11:12 +0200

    Use java.util.PriorityQueue. Faster than LinkedList and slightly faster than original Clojure implementation. Note that this doesn't return exactly the same results as the Clojure implementation.

--- src/priority_queue.clj
+++ src/priority_queue.clj
@@ -1,29 +1,17 @@
 (ns priority-queue
   (:refer-clojure :exclude [into pop])
-  (:import [clojure.string] [java.util LinkedList]))
+  (:import [clojure.string]
+           [java.util PriorityQueue]))
 
 (defn create [cmp]
   "Create an empty priority queue. Its elements will be ordered by comparing the
   result of `(key element)`."
-  {:cmp cmp :elements (LinkedList.)})
+  (PriorityQueue. 11 cmp))
 
 (defn push [queue val]
   "Push `val` in `queue`."
   (locking queue
-    (let [elements (:elements queue)
-          n        (.size elements)]
-      (loop [i 0]
-        (if (< i n)
-          (case ((:cmp queue) (.get elements i) val)
-            -1 ; old < val: keep going
-              (recur (inc i))
-             0 ; old == val: still keep going, val will be inserted AFTER
-               ; the ones it is equal to
-              (recur (inc i))
-            +1 ; old > val: insert val here
-              (.add elements i val))
-          ; all <= val: insert val at end
-          (.add elements i val))))))
+    (.add queue val)))
 
 (defn into [queue vals]
   "Push list of values `vals` into `queue`."
@@ -34,4 +22,4 @@
 (defn pop [queue]
   "Pops element from `queue` and returns it."
   (locking queue
-    (.poll (:elements queue))))
+    (.poll queue)))
