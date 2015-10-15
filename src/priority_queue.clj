bb78ec7732a92a9fdfca8a7c579dc3ab5177990e
Janwillem Swalens <jswalens@vub.ac.be>
2015-10-15 11:23:58 +0200

    Use PriorityBlockingQueue, which is synchronized, and thus an explicit lock is not required. Does not seem measurably faster, so don't use this for now.

--- src/priority_queue.clj
+++ src/priority_queue.clj
@@ -1,25 +1,22 @@
 (ns priority-queue
   (:refer-clojure :exclude [into pop])
   (:import [clojure.string]
-           [java.util PriorityQueue]))
+           [java.util.concurrent PriorityBlockingQueue]))
 
 (defn create [cmp]
   "Create an empty priority queue. Its elements will be ordered by comparing the
   result of `(key element)`."
-  (PriorityQueue. 11 cmp))
+  (PriorityBlockingQueue. 11 cmp))
 
 (defn push [queue val]
   "Push `val` in `queue`."
-  (locking queue
-    (.add queue val)))
+  (.add queue val))
 
 (defn into [queue vals]
   "Push list of values `vals` into `queue`."
-  (locking queue
-    (doseq [v vals]
-      (push queue v))))
+  (doseq [v vals]
+    (push queue v)))
 
 (defn pop [queue]
   "Pops element from `queue` and returns it."
-  (locking queue
-    (.poll queue)))
+  (.poll queue))
