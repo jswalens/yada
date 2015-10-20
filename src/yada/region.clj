9112c28aed83e06c2c7d48e3c9112ef75225d309
Janwillem Swalens <jswalens@vub.ac.be>
2015-10-20 13:36:52 +0200

    Rewrite grow-region to be more natural code, using for instead of reduce.

--- src/yada/region.clj
+++ src/yada/region.clj
@@ -57,34 +57,31 @@
        (count borders))
      new-bad-elements]))
 
-(defnp visit-neighbors [current center-element visited borders edge-map]
+(defnp visit-neighbors [current center-element visited]
   (let [neighbors         (:neighbors @current)
         boundary?         (= (element/get-num-edge center-element) 1)
-        center-coordinate (element/get-new-point center-element)]
-    (log "Visiting neighbors...")
-    (reduce-all
-      (fn [{:keys [encroached to-expand borders edge-map] :as m} neighbor]
-        (log "Visiting neighbor " (element/element->str neighbor))
-        (if (element/in-circum-circle? neighbor center-coordinate)
-          ; This element is part of the region:
-          (if (and (not boundary?) (= (element/get-num-edge neighbor) 1))
-            ; It encroaches on the mesh boundary, so we'll have to split it and
-            ; restart:
-            (assoc m :encroached neighbor)
-            ; Continue breadth-first search:
-            (update-in m [:to-expand] conj neighbor))
-          ; This element is not part of the region, so it borders the region.
-          ; Save its info for retriangulation.
-          (let [border-edge (element/get-common-edge @neighbor @current)]
-            ; C version says here: if no border-edge found: restart the tx.
-            ; As far as I can see, this error is not possible in Clojure's STM.
-            (when (.contains borders border-edge)
-              (error "duplicate in borders: " border-edge))
-            (-> m
-              (update-in [:borders] conj border-edge)
-              (update-in [:edge-map] #(mesh/edge-map-put-if-empty % border-edge neighbor))))))
-      {:encroached nil :to-expand [] :borders borders :edge-map edge-map}
-      (remove #(.contains visited %) neighbors))))
+        center-coordinate (element/get-new-point center-element)
+        results
+          (for [neighbor (remove #(.contains visited %) neighbors)]
+            (if (element/in-circum-circle? neighbor center-coordinate)
+              ; This element is part of the region:
+              (if (and (not boundary?) (= (element/get-num-edge neighbor) 1))
+                ; It encroaches on the mesh boundary, so we'll have to split it and
+                ; restart:
+                {:encroached neighbor}
+                ; Continue breadth-first search:
+                {:to-expand neighbor})
+              ; This element is not part of the region, so it borders the region.
+              ; Save its info for retriangulation.
+              (let [border-edge (element/get-common-edge @neighbor @current)]
+                ; C version says here: if no border-edge found: restart the tx.
+                ; As far as I can see, this error is not possible in Clojure's STM.
+                {:border   border-edge
+                 :edge-map [border-edge neighbor]})))]
+    {:encroached       (->> results (map :encroached) (filter some?) (first))
+     :to-expand        (->> results (map :to-expand)  (filter some?))
+     :new-borders      (->> results (map :border)     (filter some?))
+     :edge-map-updates (->> results (map :edge-map)   (filter some?))}))
 
 (defnp grow-region [center-element]
   "Returns either `{:encroached encroached-neighbor}` or
@@ -92,21 +89,26 @@
   (loop [expand-queue [center-element]
          visited      #{}
          borders      #{}
-         edge-map     {}] ; edge-map of the region
+         edge-map     {}] ; (partial) edge-map of the region
     (if-let [current (first expand-queue)]
       (if (contains? visited current)
         (recur (rest expand-queue) visited borders edge-map)
         (let [new-visited (conj visited current)
-              {:keys [encroached to-expand borders edge-map]}
-                (visit-neighbors current center-element new-visited borders
-                  edge-map)]
+              {:keys [encroached to-expand new-borders edge-map-updates]}
+                (visit-neighbors current center-element new-visited)
+              new-edge-map
+                (reduce
+                  (fn [edge-map [edge element]]
+                    (mesh/edge-map-put-if-empty edge-map edge element))
+                  edge-map
+                  edge-map-updates)]
           (if encroached
             {:encroached encroached}
             (recur
               (into (rest expand-queue) to-expand) ; will never have duplicates
               new-visited
-              borders
-              edge-map))))
+              (into borders new-borders)
+              new-edge-map))))
       {:encroached nil :visited visited :borders borders :edge-map edge-map})))
 
 (defnp refine-helper [element mesh]
